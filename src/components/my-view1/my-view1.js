714dcb73e440afda9251eb22932ead3df95210fb
Roberto Sobachi <roberto.sobachi@me.com>
2017-10-17 12:52:17 +0100

    migrating first view

---
+++ src/components/my-view1/my-view1.js
@@ -0,0 +1,16 @@
+import {Element as PolymerElement}
+    from "../../../node_modules/@polymer/polymer/polymer-element.js";
+
+export class MyView1 extends PolymerElement {
+  // Define a string template instead of a `<template>` element.
+  static get template() {
+    return `<div class="card">
+      <div class="circle">1</div>
+      <h1>View One</h1>
+      <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
+      <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
+    </div>`;
+  }
+}
+
+customElements.define('my-view1', MyView1);
