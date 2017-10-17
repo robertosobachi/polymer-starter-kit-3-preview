102cfadaf666d5e7f65b998804b4a79094166b31
Roberto Sobachi <roberto.sobachi@me.com>
2017-10-17 13:00:05 +0100

    added third view

---
+++ src/components/my-view3/my-view3.js
@@ -0,0 +1,16 @@
+import {Element as PolymerElement}
+    from "../../../node_modules/@polymer/polymer/polymer-element.js";
+
+export default class MyView3 extends PolymerElement {
+  // Define a string template instead of a `<template>` element.
+  static get template() {
+    return `<div class="card">
+      <div class="circle">3</div>
+      <h1>View Three</h1>
+      <p>Modus commodo minimum eum te, vero utinam assueverit per eu.</p>
+      <p>Ea duis bonorum nec, falli paulo aliquid ei eum.Has at minim mucius aliquam, est id tempor laoreet.Pro saepe pertinax ei, ad pri animal labores suscipiantur.</p>
+    </div>`;
+  }
+}
+
+customElements.define('my-view3', MyView3);
